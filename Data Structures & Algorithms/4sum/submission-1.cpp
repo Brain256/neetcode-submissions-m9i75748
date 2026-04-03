036c964330c40bf80038b799f51bca7336985e9b
Brian Yin <74633320+Brain256@users.noreply.github.com>
2026-04-03 17:31:21 -0400

    Add: 4sum - submission-1

---
+++ Data Structures & Algorithms/4sum/submission-1.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+         sort(nums.begin(), nums.end()); 
+        vector<vector<int>> res; 
+
+        if(nums.size() < 4) {
+            return {}; 
+        }
+
+        for(int i = 0; i < nums.size(); ++i) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for(int j = i+1; j < nums.size()-2; ++j) {
+
+                if (j > i+1 && nums[j] == nums[j - 1]) continue; 
+
+                int t = nums[i] + nums[j]; 
+
+                int l = j+1; 
+                int r = nums.size()-1; 
+
+                while(l < r) {
+
+                    long sum = t + (long)nums[l] + (long)nums[r]; 
+
+                    if(sum < target) {
+                        l++; 
+                    } else if(sum > target) {
+                        r--; 
+                    } else {
+                        res.push_back({nums[i], nums[j], nums[l], nums[r]}); 
+
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        
+                        l++; 
+                        r--; 
+                    }
+                }
+            }
+        }
+
+        return res; 
+    }
+};
