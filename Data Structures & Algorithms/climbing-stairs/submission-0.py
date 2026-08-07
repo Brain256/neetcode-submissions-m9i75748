75eccc145cd91b559cd3d36a55309bfc11748871
Brian Yin <74633320+Brain256@users.noreply.github.com>
2026-08-06 21:25:52 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+
+        def recurse(n): 
+
+
+            if n == 0: 
+                return 1
+            
+            if n < 0: 
+                return 0
+
+            if n in memo: 
+                return memo[n]
+
+            memo[n] = recurse(n-1) + recurse(n-2)
+
+            return memo[n]
+        
+        return recurse(n)
